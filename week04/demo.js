f90ed9cce862553662cc0021406e6169bfbdb73b
alwynzhou <alwynzhou@webank.com>
2020-05-06 00:52:38 +0800

    add

---
+++ week04/demo.js
@@ -0,0 +1,28 @@
+// setTimeout(() => {
+//   console.log(2)
+// }, 2)
+
+// setTimeout(() => {
+//   console.log(1)
+// }, 0)
+
+
+// // new Promise(res => res())
+// //   .then(() => {
+// //     set
+// //   })
+async function async1() {
+  console.log('async1 start');
+  await async2();
+  console.log('async1 end');
+}
+async function async2() {
+  console.log('async2');
+}
+async1();
+new Promise(function (resolve) {
+  console.log('promise1');
+  resolve();
+}).then(function () {
+  console.log('promise2');
+});
